c2e70478cce7eaeb434efe449ad06838fcb2b95b
Anayat Khan <anayat0khan@gmail.com>
2024-02-12 21:59:02 +0530

    setup 1

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <section className='py-24'>
       <div className='container'>
-        <h1 className='text-3xl font-bold'>Next TS Starter</h1>
+        <h1 className='text-3xl font-bold'>t TS Starter</h1>
       </div>
     </section>
   )
