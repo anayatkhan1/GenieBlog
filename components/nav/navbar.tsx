5f10fd0d070ab49973d3b7581c09a26ed232eba8
Anayat Khan <anayat0khan@gmail.com>
2024-02-23 19:14:00 +0530

    connect supabase db & fix navbar

--- components/nav/navbar.tsx
+++ components/nav/navbar.tsx
@@ -13,9 +13,7 @@ const Navbar = () => {
                         width={50}
                         height={50}
                     />
-                    <span className="animate-background-shine inline-flex bg-[linear-gradient(110deg,#939393,45%,#1e293b,55%,#939393)]  bg-[length:250%_100%] bg-clip-text text-2xl font-semibold text-black text-transparent">
-                        Genie Blog
-                    </span>
+                    <span className="text-2xl font-bold">Genie Blog</span>
                 </Link>
                 <Button size="lg">
                     <span className="font-semibold ">Sign in</span>
