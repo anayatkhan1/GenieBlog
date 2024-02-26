b780ae2f9db391ba449d17d9fd8bb29b1ebd93b3
Anayat Khan <anayat0khan@gmail.com>
2024-02-26 22:34:22 +0530

    vercel duration

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,7 +1,7 @@
 import AllBlogs from "@/components/all-blogs";
 import Generate from "@/components/generate";
 
-export const maxDuration = 15;
+export const maxDuration = 10;
 const Home = async () => {
     return (
         <section className="py-24">
