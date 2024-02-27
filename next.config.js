ecd50d4808ec1590fd556af301b94928f5313542
Anayat Khan <anayat0khan@gmail.com>
2024-02-27 11:54:34 +0530

    serverless setup

--- next.config.js
+++ next.config.js
@@ -10,4 +10,14 @@ const nextConfig = {
     },
 };
 
-module.exports = nextConfig;
+module.exports = {
+    nextConfig,
+    env: {
+        SUPABASE_URL: process.env.SUPABASE_URL,
+        SUPABASE_SERVICE_KEY: process.env.SUPABASE_SERVICE_KEY,
+        OPENAI_API_KEY: process.env.OPENAI_API_KEY,
+        CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
+        NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY:
+            process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
+    },
+};
